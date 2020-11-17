287e4e1b755df8a840173f812dcbeb5f5bef1f4b
Tomasz (ttr) Rudzki <ttr@ttrsite.net>
2020-11-17 00:04:15 +0000

    another BP fx for useLine

--- src/ArrowMod.cs
+++ src/ArrowMod.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 namespace ArrowMod {
     internal class ArrowMod : MelonMod
     {
-
         public override void OnApplicationStart()
         {
             Debug.Log($"[{InfoAttribute.Name}] Version {InfoAttribute.Version} loaded!");
             Settings.OnLoad();
         }
-
-
-
     }
 }
--- src/Patches.cs
+++ src/Patches.cs
@@ -14,23 +14,38 @@ namespace ArrowMod
             [HarmonyPatch(typeof(Panel_Crafting), "ItemPassesFilter")]
             private static class RecipesInToolsRecipes
             {
-                internal static void Prefix(Panel_Crafting __instance, BlueprintItem bpi)
+                internal static void Postfix(Panel_Crafting __instance, BlueprintItem bpi)
                 {
                     if (bpi?.m_CraftedResult?.name == "GEAR_Arrow")
                     {
                         bpi.m_DurationMinutes = Settings.options.arrowCraftTime;
                         if (Settings.options.arrowUseLine)
                         {
-                            bpi.m_RequiredGear = new Il2CppReferenceArray<GearItem>(3) { [0] = GetGearItemPrefab("GEAR_Line"),  [1] = GetGearItemPrefab("GEAR_CrowFeather"), [2] = GetGearItemPrefab("GEAR_ArrowShaft") };
-                            bpi.m_RequiredGearUnits = new Il2CppStructArray<int>(3) { [0] = 1, [1] = 3, [2] = 1 };
+                            bpi.m_RequiredGear[0] = GetGearItemPrefab("GEAR_Line");
+                            bpi.m_RequiredGear[1] = GetGearItemPrefab("GEAR_CrowFeather");
+                            bpi.m_RequiredGear[2] = GetGearItemPrefab("GEAR_ArrowShaft");
+                            bpi.m_RequiredGearUnits[0] = 1;
+                            bpi.m_RequiredGearUnits[1] = 3;
+                            bpi.m_RequiredGearUnits[2] = 1;
                         }
                     } else if (bpi?.m_CraftedResult?.name == "GEAR_ArrowHead")
                     {
                         bpi.m_DurationMinutes = Settings.options.arrowHeadCraftTime;
                     } else if (bpi?.m_CraftedResult?.name == "GEAR_ArrowShaft" && Settings.options.arrowUseLine)
                     {
-                        bpi.m_RequiredGear = new Il2CppReferenceArray<GearItem>(2) { [0] = GetGearItemPrefab("GEAR_BirchSaplingDried"), [1] = GetGearItemPrefab("GEAR_ArrowHead")};
-                        bpi.m_RequiredGearUnits = new Il2CppStructArray<int>(2) { [0] = 1, [1] = 3};
+                        if (bpi.m_RequiredGear.Count != 2)
+                        {
+                            MelonLogger.Log(bpi.m_RequiredGear.Count);
+                            bpi.m_RequiredGear = new Il2CppReferenceArray<GearItem>(2) { [0] = GetGearItemPrefab("GEAR_BirchSaplingDried"), [1] = GetGearItemPrefab("GEAR_ArrowHead") };
+                            bpi.m_RequiredGearUnits = new Il2CppStructArray<int>(2) { [0] = 1, [1] = 3 };
+                        }
+                        else
+                        {
+                            bpi.m_RequiredGear[0] = GetGearItemPrefab("GEAR_BirchSaplingDried");
+                            bpi.m_RequiredGear[1] = GetGearItemPrefab("GEAR_ArrowHead");
+                            bpi.m_RequiredGearUnits[0] = 1;
+                            bpi.m_RequiredGearUnits[1] = 3;
+                        }
 
                     }
 
